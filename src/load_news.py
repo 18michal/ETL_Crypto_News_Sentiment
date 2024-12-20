4b89c6df93c8b21852d423859bbc336d9586769b
18michal <18janowski@gmail.com>
2024-12-20 21:34:09 +0100

    try, except added

--- src/load_news.py
+++ src/load_news.py
@@ -1,8 +1,13 @@
+from pyodbc import IntegrityError  # pylint: disable=E0611
+
 from src.db_utils_helper import get_connection
 
 
-def load_data_to_db(data: list[dict[str, str]]) -> None:
-    """Load data from a list of dictionaries into the database."""
+def load_data_to_db(data: list[dict[str, str]]):
+    """
+    Load data from a list of dictionaries into the database.
+    Skips duplicate records automatically due to the primary key constraint.
+    """
 
     conn = get_connection()
     cursor = conn.cursor()
@@ -13,15 +18,21 @@ def load_data_to_db(data: list[dict[str, str]]) -> None:
     """
 
     for row in data:
-        cursor.execute(
-            insert_query,
-            row.get("date"),
-            row.get("title"),
-            row.get("link"),
-            row.get("content"),
-            row.get("preprocessed_content"),
-            row.get("sentiment"),
-        )
+        if row["title"] is not None and row["date"] is not None:
+            try:
+                cursor.execute(
+                    insert_query,
+                    row.get("Date"),
+                    row.get("Title"),
+                    row.get("Link"),
+                    row.get("Content"),
+                    row.get("PreprocessedContent"),
+                    row.get("Sentiment"),
+                )
+            except IntegrityError:
+                print(
+                    f"Skipping {row.get('Title')} ({row.get('Date')}) as it already exists in the database."
+                )
 
     conn.commit()
     conn.close()
